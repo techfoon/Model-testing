a6b2d002b2680040df4042d2a7c70a5d0dada724
techfoon <techfoon.com@gmail.com>
2024-08-24 11:15:09 +0530

    well done

--- lib/modeluser.dart
+++ lib/modeluser.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+//map to model
 class Modeluser {
   String? id, name, img;
 
@@ -9,10 +9,19 @@ class Modeluser {
   factory Modeluser.fromMap(Map<String, dynamic> map1) {
     return Modeluser(id: map1['id'], name: map1['name'],img: map1['img']);
   }
+
+  // From model to map
+/*
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'img': img,
+    };
+  }*/
 }
-//map to model
 
 
 
 
-//model to map
+
