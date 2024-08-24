92ee9ab6d62b3a5d9cfbc6760ff06c65ddfa113e
techfoon <techfoon.com@gmail.com>
2024-08-24 10:26:06 +0530

    good to go

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:usermodel_with_crud/myhttp.dart';
-import 'package:usermodel_with_crud/modeluser.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Dashborad1(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: UserDash(),
-    );
-  }
-}
-
-class UserDash extends StatelessWidget {
+class Dashborad1 extends StatelessWidget {
+  List<Map<String, dynamic>> mData = [
+    {
+      'id': '123',
+      'name': 'aman',
+      'img': 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
+    },
+    {
+      'id': '134',
+      'name': 'rahul',
+      'img': 'https://cdn-icons-png.flaticon.com/512/149/149072.png'
+    },
+    {
+      'id': '12323',
+      'name': 'nitin',
+      'img': 'https://cdn-icons-png.flaticon.com/512/149/149073.png'
+    },
+    {
+      'id': '12344',
+      'name': 'ashok',
+      'img': 'https://cdn-icons-png.flaticon.com/512/149/149074.png'
+    },
+    {
+      'id': '1243',
+      'name': 'aman',
+      'img': 'https://cdn-icons-png.flaticon.com/512/149/149075.png'
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("CRUD"),
-        ),
-        body: ListView.builder(
-          itemBuilder: (_, index) {
-            var eachUserModel = UserModel.fromMap(AppContants.mData[index]);
-            return ListTile(
-              leading: Image.network(eachUserModel.img ?? "https://cdn-icons-png.flaticon.com/512/149/149077.png"),
-              trailing: IconButton(
-                iconSize: 23,
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  print("button is taped");
-                },
-              ),
-              title: Text(eachUserModel.name ?? "name issues"),
-              subtitle: Text(eachUserModel.id.toString()),
-            );
-          },
-          itemCount: AppContants.mData.length,
-        ));
+      body: ListView.builder(itemBuilder: (_, index) {
+        ListTile(
+          trailing: IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+          title: mData[index]['name'],
+          subtitle: mData[index]['id'],
+        );
+      }),
+    );
   }
 }
